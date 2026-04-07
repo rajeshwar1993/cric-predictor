3aa5e7e6215fdb232bae35b8373e6e694bc460c2
Rajeshwar Rudra <rajeshwar1993@gmail.com>
2026-04-07 23:25:44 +0530

    Delete 20260407000011_fix_fixture_match_number.sql

--- supabase-2/supabase/migrations/20260407000011_fix_fixture_match_number.sql
+++
@@ -1,6 +0,0 @@
--- Drop the unique constraint on (season_id, match_number) since Sportmonks
--- playoff fixtures can share match_number with regular season fixtures.
--- The api_id UNIQUE constraint is sufficient for deduplication.
-
-ALTER TABLE v2_league_season_fixtures
-  DROP CONSTRAINT IF EXISTS v2_league_season_fixtures_season_id_match_number_key;
